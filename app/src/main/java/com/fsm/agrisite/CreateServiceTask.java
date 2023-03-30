262853d6da9b95bb08ea5cd84ca45f0dc76cb27f
nethli <asleeish2000@gmail.com>
2023-03-30 18:59:41 +0530

    Admin Dashboard and the Login Page

---
+++ app/src/main/java/com/fsm/agrisite/CreateServiceTask.java
@@ -0,0 +1,14 @@
+package com.fsm.agrisite;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreateServiceTask extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_service_task);
+    }
+}
